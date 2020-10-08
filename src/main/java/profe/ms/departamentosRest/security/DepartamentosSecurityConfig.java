9ed0b6d26dc76e15446202a38062057dd8fe1322
made <kike@mazarredos.com>
2020-10-08 08:55:52 +0200

    Certificado en cliente browser
    El cliente browser tiene que presentar un certificado para poder acceder
    al
    servicio

--- src/main/java/profe/ms/departamentosRest/security/DepartamentosSecurityConfig.java
+++ src/main/java/profe/ms/departamentosRest/security/DepartamentosSecurityConfig.java
@@ -1,49 +1,42 @@
 package profe.ms.departamentosRest.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @EnableWebSecurity
 public class DepartamentosSecurityConfig  extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
-    }
-
-    /**
-     * subjectPrincipalRegex("CN=(.*?)(?:,|$)") :- The regular expression used to extract a username from the client certificate’s subject name.
-     * (CN value of the client certificate)
-     */
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //Enabling X.509 client authentication is very straightforward. just add x509()
-        http
-	        .authorizeRequests()
-	        .anyRequest().permitAll()
-/*        .x509().subjectPrincipalRegex("CN=(.*?)(?:,|$)").
-                and().authorizeRequests().anyRequest().authenticated()
-                .and().userDetailsService(userDetailsService())*/
-                ;
+        http.authorizeRequests().anyRequest().authenticated()
+          .and()
+          .x509()
+            .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+            .userDetailsService(userDetailsService());
+    }
+ 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return (username -> {
+        	/* Si queremos autorizar a cualquiera que tenga el certificado */
+            if (username != null) {
+                return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+            }
+            /* Si queremos discriminar por usuario (CN) */
+        	/*if (username.equals("Bob")) {
+                return new User(username, "", 
+                  AuthorityUtils
+                    .commaSeparatedStringToAuthorityList("ROLE_USER"));
+            }*/
+            throw new UsernameNotFoundException("User not found!");
+        });
     }
 
-
- /*   @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public User loadUserByUsername(String username) {
-                if (username != null) {
-                    return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
-                }
-                return null;
-            }
-        };
-    }*/
 }
